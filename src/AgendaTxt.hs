c856a6aa508d5e8972051828e450d4bbc5e1bb19
Jasper Woudenberg <mail@jasperwoudenberg.com>
2023-10-06 23:23:41 +0200

    Add parser for event line

--- src/AgendaTxt.hs
+++ src/AgendaTxt.hs
@@ -1,4 +1,125 @@
-module AgendaTxt (main) where
+module AgendaTxt where
+
+import Chronos
+import Data.Attoparsec.Text
+import Data.Char (isAlpha)
+import Data.Scientific (floatingOrInteger)
+import Data.Text
 
 main :: IO ()
 main = putStrLn "Hello, agenda-txt!"
+
+data RepeatFilter
+  = DatePattern DatePattern
+  | NotDatePattern DatePattern
+  | WeekDay DayOfWeek
+  | EndDate Day
+  deriving (Eq, Show)
+
+data DatePattern = DatePattern_
+  { year :: Maybe Year,
+    month :: Maybe Month,
+    day :: Maybe DayOfMonth
+  }
+  deriving (Eq, Show)
+
+data EventTime = EventTime
+  { startTime :: TimeOfDay,
+    durationMinutes :: Maybe Int,
+    timezone :: Maybe Text
+  }
+  deriving (Eq, Show)
+
+data Event = Event
+  { startDay :: Day,
+    repeatRule :: Maybe [RepeatFilter],
+    time :: Maybe EventTime,
+    description :: Text
+  }
+  deriving (Eq, Show)
+
+parserEvent :: Parser Event
+parserEvent = do
+  startDate <- parserDate
+  _ <- skipSpace
+  repeatRule <- option Nothing (Just <$> parserRepeatRule)
+  _ <- skipSpace
+  time <- option Nothing (Just <$> parserTime)
+  _ <- skipSpace
+  description <- Data.Attoparsec.Text.takeWhile (/= '\n')
+  pure $ Event startDate repeatRule time description
+
+parserRepeatRule :: Parser [RepeatFilter]
+parserRepeatRule =
+  char '['
+    <* skipSpace
+    *> sepBy' parserRepeatStep skipSpace
+    <* skipSpace
+    <* char ']'
+
+parserRepeatStep :: Parser RepeatFilter
+parserRepeatStep =
+  choice
+    [ NotDatePattern <$> (char '!' *> parserDatePattern),
+      EndDate <$> (string "<=" *> parserDate),
+      DatePattern <$> parserDatePattern,
+      WeekDay <$> parserDayOfWeek
+    ]
+
+parserDatePattern :: Parser DatePattern
+parserDatePattern = do
+  year <- option Nothing (Just . Year <$> parserInt)
+  _ <- char '-'
+  month <- option Nothing (Just . Month <$> parserInt)
+  _ <- char '-'
+  day_ <- option Nothing (Just . DayOfMonth <$> parserInt)
+  pure $ DatePattern_ year month day_
+
+parserInt :: Parser Int
+parserInt = do
+  floatOrInteger <- floatingOrInteger <$> scientific
+  case floatOrInteger of
+    Left (_ :: Double) -> fail "unexpected float in DatePattern"
+    Right int ->
+      if int < 0
+        then fail "unexpected negative number"
+        else pure int
+
+parserTime :: Parser EventTime
+parserTime =
+  EventTime
+    <$> parserTimeOfDay
+    <* skipSpace
+    <*> option Nothing (Just <$> parserDuration)
+    <* skipSpace
+    <*> option Nothing (Just <$> parserTimezone)
+
+parserDuration :: Parser Int
+parserDuration = do
+  _ <- char '+'
+  hours <- parserInt
+  _ <- char ':'
+  minutes <- parserInt
+  pure (60 * hours + minutes)
+
+parserTimezone :: Parser Text
+parserTimezone =
+  char '(' *> Data.Attoparsec.Text.takeWhile isAlpha <* char ')'
+
+parserDayOfWeek :: Parser DayOfWeek
+parserDayOfWeek =
+  choice
+    [ string "mon" *> pure monday,
+      string "tue" *> pure tuesday,
+      string "wed" *> pure wednesday,
+      string "thu" *> pure thursday,
+      string "fri" *> pure friday,
+      string "sat" *> pure saturday,
+      string "sun" *> pure sunday
+    ]
+
+parserDate :: Parser Day
+parserDate = dateToDay <$> parser_Ymd (Just '-')
+
+parserTimeOfDay :: Parser TimeOfDay
+parserTimeOfDay = parser_HMS_opt_S (Just ':')
---
+++ test/Main.hs
@@ -0,0 +1,151 @@
+module Main (Main.main) where
+
+import AgendaTxt
+import Chronos hiding (day)
+import Data.Attoparsec.Text (parseOnly)
+import Data.List (intersperse)
+import Data.String (fromString)
+import Data.Text (Text)
+import qualified Data.Text.Lazy
+import Data.Text.Lazy.Builder (Builder)
+import qualified Data.Text.Lazy.Builder as Builder
+import Hedgehog
+import Hedgehog.Gen as Gen
+import Hedgehog.Main (defaultMain)
+import Hedgehog.Range as Range
+
+main :: IO ()
+main =
+  defaultMain
+    [ checkParallel (Group "parsing events" [("fuzzed events", fuzzedEventsTest)])
+    ]
+
+fuzzedEventsTest :: Property
+fuzzedEventsTest = property $ do
+  event <- forAll genEvent
+  eventLine <- forAll (builderEvent event)
+  let parseResult = parseOnly parserEvent $ Data.Text.Lazy.toStrict (Builder.toLazyText eventLine)
+  case parseResult of
+    Left message -> do
+      annotateShow message
+      failure
+    Right parsedEvent ->
+      event === parsedEvent
+
+builderEvent :: Event -> Gen Builder
+builderEvent event =
+  builderDay (startDay event)
+    <> builderSpacing
+    <> Prelude.maybe mempty builderRepeatRule (repeatRule event)
+    <> builderSpacing1
+    <> Prelude.maybe mempty builderTime (time event)
+    <> builderSpacing
+    <> pure (Builder.fromText (description event))
+
+builderSpacing :: Gen Builder
+builderSpacing = Builder.fromText <$> Gen.text (Range.linear 0 2) (pure ' ')
+
+builderSpacing1 :: Gen Builder
+builderSpacing1 = Builder.fromText <$> Gen.text (Range.linear 1 2) (pure ' ')
+
+builderDay :: Day -> Gen Builder
+builderDay day =
+  pure $ builder_Ymd (Just '-') (dayToDate day)
+
+builderRepeatRule :: [RepeatFilter] -> Gen Builder
+builderRepeatRule repeatFilters =
+  pure "["
+    <> builderSpacing
+    <> mconcat (intersperse builderSpacing1 $ fmap builderRepeatFilter repeatFilters)
+    <> builderSpacing
+    <> pure "]"
+
+builderRepeatFilter :: RepeatFilter -> Gen Builder
+builderRepeatFilter repeatFilter =
+  case repeatFilter of
+    DatePattern pattern -> builderDatePattern pattern
+    NotDatePattern pattern -> pure "!" <> builderDatePattern pattern
+    WeekDay weekDay ->
+      pure $
+        caseDayOfWeek
+          (buildDayOfWeekMatch "sun" "mon" "tue" "wed" "thu" "fri" "sat")
+          weekDay
+    EndDate day -> pure "<=" <> builderDay day
+
+builderDatePattern :: DatePattern -> Gen Builder
+builderDatePattern datePattern =
+  Prelude.maybe mempty (builderInt . getYear) (year datePattern)
+    <> pure "-"
+    <> Prelude.maybe mempty (builderInt . getMonth) (month datePattern)
+    <> pure "-"
+    <> Prelude.maybe mempty (builderInt . getDayOfMonth) (day datePattern)
+
+builderInt :: Int -> Gen Builder
+builderInt = pure . fromString . show
+
+builderTime :: EventTime -> Gen Builder
+builderTime eventTime =
+  builderTimeOfDay (startTime eventTime)
+    <> builderSpacing
+    <> Prelude.maybe mempty builderDuration (durationMinutes eventTime)
+    <> builderSpacing
+    <> Prelude.maybe mempty builderTimezone (timezone eventTime)
+
+builderTimeOfDay :: TimeOfDay -> Gen Builder
+builderTimeOfDay timeOfDay =
+  pure $ builder_HMS SubsecondPrecisionAuto (Just ':') timeOfDay
+
+builderDuration :: Int -> Gen Builder
+builderDuration minutes =
+  pure "+"
+    <> builderInt (minutes `div` 60)
+    <> pure ":"
+    <> builderInt (minutes `mod` 60)
+
+builderTimezone :: Text -> Gen Builder
+builderTimezone timezone =
+  pure $ "(" <> Builder.fromText timezone <> ")"
+
+genEvent :: Gen Event
+genEvent =
+  Event
+    <$> genDay
+    <*> Gen.maybe (list (linear 0 4) genRepeatFilter)
+    <*> Gen.maybe genEventTime
+    <*> text (linear 0 20) unicode
+
+genDay :: Gen Day
+genDay = Day <$> int (linear 0 10000)
+
+genRepeatFilter :: Gen RepeatFilter
+genRepeatFilter =
+  choice
+    [ DatePattern <$> genDatePattern,
+      NotDatePattern <$> genDatePattern,
+      WeekDay <$> genDayOfWeek,
+      EndDate <$> genDay
+    ]
+
+genDatePattern :: Gen DatePattern
+genDatePattern =
+  DatePattern_
+    <$> Gen.maybe (Year <$> int (Range.linear 1800 2400))
+    <*> Gen.maybe (Month <$> int (Range.linear 0 11))
+    <*> Gen.maybe (DayOfMonth <$> int (Range.linear 0 31))
+
+genDayOfWeek :: Gen DayOfWeek
+genDayOfWeek = element [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
+
+genEventTime :: Gen EventTime
+genEventTime =
+  EventTime
+    <$> genTimeOfDay
+    <*> Gen.maybe (int (Range.linear 0 1400))
+    <*> Gen.maybe (text (Range.linear 2 4) upper)
+
+genTimeOfDay :: Gen TimeOfDay
+genTimeOfDay =
+  TimeOfDay
+    <$> Gen.int (Range.linear 0 23)
+    <*> Gen.int (Range.linear 0 59)
+    <*> pure 0
